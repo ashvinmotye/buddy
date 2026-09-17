39b402f1853365983d43224860ca08898ad06a51
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2026-09-17 16:03:34 +0400

    Set reminder url

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'buddy-5';
+const CACHE = 'buddy-6';
 const ASSETS = ['./','./index.html','./app.css','./tax.js','./reminder-config.js','./reminders.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));});
 self.addEventListener('activate', event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));});
